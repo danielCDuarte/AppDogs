d7181cc8a5fc7b0210fa22e2e7be3e4f3626f73b
Daniel <daniel38_14@hotmail.com>
2025-06-06 10:44:44 -0500

    Initial Commit

---
+++ AppDogs/AppDogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDogsApp.swift
+//  AppDogs
+//
+//  Created by Daniel Crespo Duarte on 6/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AppDogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
