ec05eb46d1c54284ecc1822e460b32c83ff91996
Daniel <daniel38_14@hotmail.com>
2025-06-06 11:51:57 -0500

    add structure project

--- AppDogs/AppDogsApp.swift
+++ AppDogs/AppDogsApp.swift
@@ -11,7 +11,6 @@ import SwiftUI
 struct AppDogsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
         }
     }
 }
---
+++ AppDogs/Utils/DataConstants.swift
@@ -0,0 +1,23 @@
+//
+//  DataConstants.swift
+//  AppDogs
+//
+//  Created by Daniel Crespo Duarte on 6/06/25.
+//
+
+import Foundation
+
+struct DataConstants {
+    enum APIClient {
+        static let contentType: String = "Content-Type"
+    }
+    
+    enum InnerConstants {
+        static let applicationJson: String = "application/json"
+    }
+    struct Ws {
+        static let getDogs: String = "1151549092634943488"
+    }
+    
+    static let baseUrl: String = "hOps://jsonblob.com/api/"
+}
